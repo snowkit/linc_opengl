78682f450af7957d9b2ab0061065e07b0bf30401
underscorediscovery <sven@underscorediscovery.com>
2015-08-29 05:51:53 -0230

    add linc files

---
+++ linc/linc_opengl.h
@@ -0,0 +1,17 @@
+#ifndef _LINC_OPENGL_H_
+#define _LINC_OPENGL_H_
+
+#include <hxcpp.h>
+
+#include "../lib/glew/include/GL/glew.h"
+
+namespace linc {
+
+    namespace opengl {
+
+
+    } //opengl
+
+} //linc namespace
+
+#endif
