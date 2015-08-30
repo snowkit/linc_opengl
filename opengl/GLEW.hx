29e5ce117e97f68be44dffee819344fd406be599
underscorediscovery <sven@underscorediscovery.com>
2015-08-30 01:50:53 -0230

    add glew versions

--- opengl/GLEW.hx
+++ opengl/GLEW.hx
@@ -29,4 +29,62 @@ extern class GLEW {
     @:native('glewIsSupported')
     static function isSupported(name:String):Bool;
 
+    static var VERSION_1_1 (get, never) : Bool;
+    static inline function get_VERSION_1_1() : Bool return untyped __cpp__('GLEW_VERSION_1_1');
+
+    static var VERSION_1_2 (get, never) : Bool;
+    static inline function get_VERSION_1_2() : Bool return untyped __cpp__('GLEW_VERSION_1_2');
+
+    static var VERSION_1_2_1 (get, never) : Bool;
+    static inline function get_VERSION_1_2_1() : Bool return untyped __cpp__('GLEW_VERSION_1_2_1');
+
+    static var VERSION_1_3 (get, never) : Bool;
+    static inline function get_VERSION_1_3() : Bool return untyped __cpp__('GLEW_VERSION_1_3');
+
+    static var VERSION_1_4 (get, never) : Bool;
+    static inline function get_VERSION_1_4() : Bool return untyped __cpp__('GLEW_VERSION_1_4');
+
+    static var VERSION_1_5 (get, never) : Bool;
+    static inline function get_VERSION_1_5() : Bool return untyped __cpp__('GLEW_VERSION_1_5');
+
+    static var VERSION_2_0 (get, never) : Bool;
+    static inline function get_VERSION_2_0() : Bool return untyped __cpp__('GLEW_VERSION_2_0');
+
+    static var VERSION_2_1 (get, never) : Bool;
+    static inline function get_VERSION_2_1() : Bool return untyped __cpp__('GLEW_VERSION_2_1');
+
+    static var VERSION_3_0 (get, never) : Bool;
+    static inline function get_VERSION_3_0() : Bool return untyped __cpp__('GLEW_VERSION_3_0');
+
+    static var VERSION_3_1 (get, never) : Bool;
+    static inline function get_VERSION_3_1() : Bool return untyped __cpp__('GLEW_VERSION_3_1');
+
+    static var VERSION_3_2 (get, never) : Bool;
+    static inline function get_VERSION_3_2() : Bool return untyped __cpp__('GLEW_VERSION_3_2');
+
+    static var VERSION_3_3 (get, never) : Bool;
+    static inline function get_VERSION_3_3() : Bool return untyped __cpp__('GLEW_VERSION_3_3');
+
+    static var VERSION_4_0 (get, never) : Bool;
+    static inline function get_VERSION_4_0() : Bool return untyped __cpp__('GLEW_VERSION_4_0');
+
+    static var VERSION_4_1 (get, never) : Bool;
+    static inline function get_VERSION_4_1() : Bool return untyped __cpp__('GLEW_VERSION_4_1');
+
+    static var VERSION_4_2 (get, never) : Bool;
+    static inline function get_VERSION_4_2() : Bool return untyped __cpp__('GLEW_VERSION_4_2');
+
+    static var VERSION_4_3 (get, never) : Bool;
+    static inline function get_VERSION_4_3() : Bool return untyped __cpp__('GLEW_VERSION_4_3');
+
+    static var VERSION_4_4 (get, never) : Bool;
+    static inline function get_VERSION_4_4() : Bool return untyped __cpp__('GLEW_VERSION_4_4');
+
+    static var VERSION_4_5 (get, never) : Bool;
+    static inline function get_VERSION_4_5() : Bool return untyped __cpp__('GLEW_VERSION_4_5');
+
 }
+
+
+
+
