ca534213af597d7293557304c73a441362d50a74
underscorediscovery <sven@underscorediscovery.com>
2015-11-29 22:22:46 -0330

    fix iOS glew usage

--- linc/linc_opengl.h
+++ linc/linc_opengl.h
@@ -6,6 +6,9 @@
 #if defined(HX_ANDROID)
 #include <GLES2/gl2.h>
 #include <GLES2/gl2ext.h>
+#elif defined(IPHONE) || defined(__IPHONEOS__)
+#include <OpenGLES/ES2/gl.h>
+#include <OpenGLES/ES2/glext.h>
 #else
 #include "../lib/glew/include/GL/glew.h"
 #endif
