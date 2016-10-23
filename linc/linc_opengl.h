273aa785a2b9e7183eebed909bfde7a237ea8ab4
underscorediscovery <sven@underscorediscovery.com>
2016-10-23 01:17:41 -0230

    include the right header on tvos

--- linc/linc_opengl.h
+++ linc/linc_opengl.h
@@ -7,7 +7,7 @@
 #if defined(HX_ANDROID)
 #include <GLES2/gl2.h>
 #include <GLES2/gl2ext.h>
-#elif defined(IPHONE) || defined(__IPHONEOS__)
+#elif defined(IPHONE) || defined(__IPHONEOS__) || defined(APPLETVOS) || defined(APPLETVSIM)
 #include <OpenGLES/ES2/gl.h>
 #include <OpenGLES/ES2/glext.h>
 #else
