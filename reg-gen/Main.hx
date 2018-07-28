9c94e23e8c700766161a0ed26c9f6e79f143e44b
Aidan <aidan.lee63@gmail.com>
2018-07-28 12:55:49 +0100

    Basic opengl command to native function conversion

--- reg-gen/Main.hx
+++ reg-gen/Main.hx
@@ -10,6 +10,8 @@ class Main
         builder = new StringBuilder();
 
         build(sys.io.File.getContent('gl.xml'));
+
+        sys.io.File.saveContent('GL.hx', builder.toString());
     }
 
     static function build(_xml : String)
@@ -18,6 +20,7 @@ class Main
         var reg = xml.firstElement();
 
         writeEnums(reg);
+        writeCommands(reg);
     }
 
     /**
@@ -66,7 +69,87 @@ class Main
                 builder.add('\t').add('inline static var $name = $value').newLine();
             }
         }
+    }
 
-        sys.io.File.saveContent('GL.hx', builder.toString());
+    /**
+     * Parses the openGL XML for 'commands' elements.
+     * Each 'commands' element contains one or more 'command' element which is a openGL function.
+     * 
+     * These 'command' elements contain the command name, return type, and all parameters and their types.
+     * The GL types are converted into the equivilent Haxe types and written as an extern function.
+     * 
+     * @param _reg XML registry to parse.
+     */
+    static function writeCommands(_reg : Xml)
+    {
+        builder.newLine();
+
+        for (element in _reg.elementsNamed('commands'))
+        {
+            for (glCommand in element.elementsNamed('command'))
+            {
+                var proto : Xml = null;
+                var params = new Array<Xml>();
+
+                // Find the proto element and all param elements
+                for (part in glCommand.elements())
+                {
+                    switch (part.nodeName)
+                    {
+                        case 'proto': proto = part;
+                        case 'param': params.push(part);
+                    }
+                }
+
+                // Get the function name, return type, and setup params.
+                var funcReturn = '';
+                var funcName   = '';
+                var funcParams = new Array<{ name : String, type : String }>();
+
+                // Find the function name and return type.
+                // If there is no ptype element in proto then proto's first child value has a non GL return type (Void or Void*).
+                for (element in proto.elements())
+                {
+                    switch (element.nodeName)
+                    {
+                        case 'name' : funcName   = element.firstChild().nodeValue;
+                        case 'ptype': funcReturn = element.firstChild().nodeValue;
+                    }
+                }
+                if (funcReturn == '') funcReturn = proto.firstChild().nodeValue;
+
+                // Convert the param elements into haxe parameters
+                for (glParam in params)
+                {
+                    var name = '';
+                    var type = '';
+
+                    for (element in glParam.elements())
+                    {
+                        switch (element.nodeName)
+                        {
+                            case 'ptype' : type = element.firstChild().nodeValue;
+                            case 'name'  : name = element.firstChild().nodeValue;
+                        }
+                    }
+
+                    funcParams.push({ name : name, type : type });
+                }
+
+                // Write the haxe equivilent function
+                builder.newLine();
+                builder.add('\t').add('@:native(\'$funcName\')').newLine();
+                builder.add('\t').add('static function $funcName(');
+
+                for (i in 0...funcParams.length)
+                {
+                    builder.add(funcParams[i].name).add(' : ').add(funcParams[i].type);
+
+                    if (i != funcParams.length - 1) builder.add(', ');
+                }
+
+                builder.add(') : $funcReturn;').newLine();
+            }
+        }
     }
 }
